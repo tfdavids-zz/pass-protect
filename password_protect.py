2cbc206492c1e862e8594095db32a7bd44ebb6f7
Thomas Davids <tfdavids@gmail.com>
2014-03-25 13:23:56 -0700

    Added support for username

--- password_protect.py
+++ password_protect.py
@@ -14,25 +14,25 @@ character_set = string.ascii_letters + string.digits + '+$'
 def initialize_db():
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS t
-                 (account text, username text, salt text, modified text)''')
+                 (account text, username text, salt text, length int, modified text)''')
 
 def create_salt(length=16):
     return "".join([character_set[ord(c) % 64] for c in os.urandom(int(length))])
 
-def store_salt(name, username, salt):
+def store_salt(name, username, salt, length):
     c = conn.cursor()
     time = str(datetime.now())
-    entry = (name, username, salt, time)
+    entry = (name, username if username else 'NULL', salt, length, time)
 
-    c.execute("INSERT INTO t VALUES (?,?,?,?)", entry)
+    c.execute("INSERT INTO t VALUES (?,?,?,?,?)", entry)
     conn.commit()
 
 def get_password(name, master_password):
     c = conn.cursor()
     t = (name, )
-    c.execute('SELECT salt, username FROM t WHERE account=?', t)
-    (salt, username) = c.fetchone()
-    return (name, username, ''.join([character_set[ord(c) % 64] for c in hashlib.sha256(salt + '--' + master_password).digest()]))
+    c.execute('SELECT salt, username, length FROM t WHERE account=?', t)
+    (salt, username, length) = c.fetchone()
+    return (name, username, ''.join([character_set[ord(c) % 64] for c in hashlib.sha256(salt + '--' + master_password).digest()])[:length] )
 
 def delete_password(name):
     c = conn.cursor()
@@ -44,23 +44,29 @@ def show_passwords(name):
     c = conn.cursor()
     t = ('%' + name + '%', )
     for row in c.execute('SELECT * FROM t WHERE account LIKE ?', t):
-        print "%s (username: %s)" % (row[0], row[1])
+        if row[1] != 'NULL':
+            print "%s (username: %s)" % (row[0], row[1])
+        else:
+            print "%s" % row[0]
 
-def create_password(name, username=None):
+def create_password(name, username=None, length=16):
     c = conn.cursor()
     salt = create_salt()
-    store_salt(name, username, salt)
+    store_salt(name, username, salt, length)
 
 def print_password(name, master_password):
     (account, username, password) = get_password(name, master_password)
     print "Password for %s" % account
-    # print "Username: %s" % username
+    if username != 'NULL':
+        print "Username: %s" % username
     print "Password: %s" % password
 
-def password_exists(name):
+def password_exists(name, username):
     c = conn.cursor()
-    t = ('%' + name + '%', )
-    c.execute('SELECT account FROM t WHERE account LIKE ?', t)
+    if not username:
+        username = 'NULL'
+    t = ('%' + name + '%', username)
+    c.execute('SELECT account FROM t WHERE account LIKE ? and username = ?', t)
     if c.fetchone():
         return True
     else:
@@ -72,22 +78,52 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='texty text')
     parser.add_argument('name')
     parser.add_argument('-n', '--length', type=int)
-    parser.add_argument('-u', '--update', action='store_true')
+    parser.add_argument('-u', '--username')
+    parser.add_argument('-m', '--modify', action='store_true')
     parser.add_argument('-d', '--delete', action='store_true')
     parser.add_argument('-l', '--list', dest='lists', action='store_true')
     args = parser.parse_args()
 
-    master_password = getpass.getpass("Please enter your master password: ")
-
+    # delete a password
     if args.delete:
+        try:
+            print "Are you sure you want to delete your password named \"%s\"? This can't be undone! Press Enter to continue, or Ctrl-C to exit." % args.name
+            sys.stdin.readline()
+        except KeyboardInterrupt:
+            sys.exit(0)
         delete_password(args.name)
+
+    # show all passwords
     elif args.lists:
         show_passwords(args.name)
-    elif args.update:
+
+    # modify password
+    elif args.modify:
+        if not password_exists(args.name, args.username):
+            print "Error: couldn't find a password named \"%s\". No action was taken." % args.name
+            sys.exit(0)
+        else:
+            try:
+                print "Are you sure you want to create a new password for \"%s\"? This can't be undone! Press Enter to continue, or Ctrl-C to exit." % args.name
+                sys.stdin.readline()
+            except KeyboardInterrupt:
+                sys.exit(0)
         delete_password(args.name)
         create_password(args.name)
+        master_password = getpass.getpass("Please enter your master password: ")
         print_password(args.name, master_password)
+
+    # create/retrieve password
     else:
-        if not password_exists(args.name):
-            create_password(args.name)
+        if not password_exists(args.name, args.username):
+            try:
+                print "No password found. To create a password named \"%s\", type a username (optional), and then press enter." % args.name
+                sys.stdin.readline()
+            except KeyboardInterrupt:
+                sys.exit(0)
+            if args.length:
+                create_password(args.name, args.username, args.length)
+            else:
+                create_password(args.name, args.username)
+        master_password = getpass.getpass("Please enter your master password: ")
         print_password(args.name, master_password)
